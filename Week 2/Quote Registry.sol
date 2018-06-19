04469c33f0ddbbb1d3f0c81695870803957f63a8
Anurag Dashputre <anurag4u80@gmail.com>
2018-06-19 14:54:28 +0530

    Create Quote Registry.sol

---
+++ Week 2/Quote Registry.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.17;
+
+contract Quote_Registry {
+    mapping(string => address) quoteRegistry;
+    address public contractOwner;
+    
+    function Quote_Registry() public {
+        contractOwner = msg.sender;       
+    }
+    
+    function register(string _quote) public {
+        quoteRegistry[_quote] = msg.sender;
+    }
+
+    function ownership(string _quote) public view returns (address) {
+        return quoteRegistry[_quote];
+    }
+
+    function transfer(string _quote, address _newOwner) public payable {
+        require(msg.value == 0.5 ether,"Fee of 0.5 ether to be paid for transfer of ownership");
+        
+        address _oldOwner = quoteRegistry[_quote];
+        quoteRegistry[_quote] = _newOwner;
+        _oldOwner.transfer(msg.value);
+    }
+
+    function owner() public view returns (address) {
+        return contractOwner;
+    }
+    
+    function checkBalance(address _addr) public view returns(uint) {
+        return _addr.balance;
+    }
+}

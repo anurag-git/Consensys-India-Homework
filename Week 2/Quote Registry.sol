703bb03d28feee2f03e68bcad188361e064738f9
Anurag Dashputre <anurag4u80@gmail.com>
2018-06-19 15:21:03 +0530

    Updated register()
    - Updated register() so that quote can be registered only if it is not mapped to any address. If it is already it will throw error "Quote already registered!!!"
    - Updated constructor syntax.
    - using ownership() whereever needed, instead of "quoteRegistry[_quote]" to avoid duplicacy.

--- Week 2/Quote Registry.sol
+++ Week 2/Quote Registry.sol
@@ -1,15 +1,18 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.24;
 
 contract Quote_Registry {
     mapping(string => address) quoteRegistry;
     address public contractOwner;
     
-    function Quote_Registry() public {
+    constructor() public {
         contractOwner = msg.sender;       
     }
     
     function register(string _quote) public {
+        require(ownership(_quote) == address(0),"Quote already registered!!!");
+    
         quoteRegistry[_quote] = msg.sender;
+    
     }
 
     function ownership(string _quote) public view returns (address) {
@@ -19,8 +22,9 @@ contract Quote_Registry {
     function transfer(string _quote, address _newOwner) public payable {
         require(msg.value == 0.5 ether,"Fee of 0.5 ether to be paid for transfer of ownership");
         
-        address _oldOwner = quoteRegistry[_quote];
+        address _oldOwner = ownership(_quote);
         quoteRegistry[_quote] = _newOwner;
+        
         _oldOwner.transfer(msg.value);
     }
 
